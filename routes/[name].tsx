0bcba56a08db2e796c9e5218d7548cdf54ab988d
Emilia Zapata <zapata.contact@gmail.com>
2022-07-09 13:05:40 +1200

    au

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -15,6 +15,7 @@ async GET(_, ctx) {
   console.log(name)
   try {
     const path = `${Deno.cwd()}/articles/`;
+    console.log(Deno.cwd())
   const filePath = path + name + ".mdx";
     const article = await Deno.readFile(filePath);
     console.log('read file')
